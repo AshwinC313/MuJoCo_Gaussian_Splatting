33c1c7e0f63880204645e30a06d87a71bb64c622
Ashwin Chakravartula <cashwn@gmail.com>
2026-05-04 20:22:31 +0000

    added trajectory waypoint markers in the sim

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ SITE_NAME = "attachment_site"
 CAMERA_WRIST = "wrist_cam"
 CAMERA_TABLE = "table_cam"
 
-OBJECT_NAMES = ["apple", "banana", "blue_box", "nut_bolt"]
+OBJECT_NAMES = ["blue_box"]
 
 SCAN_HEIGHT = 0.2
 SCAN_MARGIN = 0.1
-SCAN_STEPS = 500
-RENDERER_WIDTH = 320
-RENDERER_HEIGHT = 240
+SCAN_STEPS = 1000
+RENDERER_WIDTH = 640
+RENDERER_HEIGHT = 480
 
 OUTPUT_DIR = "gaussian_splat_data"
